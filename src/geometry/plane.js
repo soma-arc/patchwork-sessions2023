a24134cd3c6cc1d1bdbc87047452367c2e3c6ecd
soma <somaarcr@gmail.com>
2023-04-04 22:35:11 +0900

    Add plane and sphere

---
+++ src/geometry/plane.js
@@ -0,0 +1,146 @@
+import Vec3 from '.,/vector3d.js';
+import Vec2 from '.,/vector2d.js';
+import Sphere from './sphere.js';
+
+export default class Plane {
+    /**
+     *
+     * @param {Vec3} p1
+     * @param {Vec3} p2
+     * @param {Vec3} p3
+     * @param {Vec3} normal
+     */
+    constructor(p1, p2, p3, normal) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.normal = normal;
+    }
+
+    /**
+     *
+     * @param {Plane} plane
+     * @returns {Plane}
+     */
+    invertOnPlane(plane) {
+        const dp = Vec3.dot(this.normal, plane.normal);
+        const nNormal = plane.normal.sub(this.normal.scale(2 * dp)).normalize();
+        return new Plane(this.invertOnPoint(plane.p1),
+                         this.invertOnPoint(plane.p2),
+                         this.invertOnPoint(plane.p3),
+                         nNormal);
+    }
+
+    /**
+     *
+     * @param {Sphere} sphere
+     * @returns {Sphere}
+     */
+    invertOnSphere(sphere) {
+        const nc = this.invertOnPoint(sphere.center);
+        return new Sphere(nc.x, nc.y, nc.z, sphere.r);
+    }
+
+    /**
+     *
+     * @param {Vec3} p
+     * @returns {Vec3}
+     */
+    invertOnPoint(p) {
+        let pos = p.sub(this.p1);
+        const dp = Vec3.dot(this.normal, pos);
+        pos = pos.sub(this.normal.scale(2 * dp));
+        return pos.add(this.p1);
+    }
+
+     /**
+     *
+     * @param {Plane} l1
+     * @param {Plane} l2
+     * @return {Vec3}
+     */
+    static computeIntersection(l1, l2) {
+        const l1Eq = Plane.computeLineEquation(new Vec2(l1.p1.x, l1.p1.z),
+                                               new Vec2(l1.p2.x, l1.p2.z));
+        const l2Eq = Plane.computeLineEquation(new Vec2(l2.p1.x, l2.p1.z),
+                                               new Vec2(l2.p3.x, l2.p3.z));
+        if (l1Eq.x === 1 && l1Eq.y === 0) {
+            return new Vec3(l1Eq.z, 0, Plane.calcY(l2Eq, l1Eq.z));
+        } else if (l1Eq.x === 0 && l1Eq.y === 1) {
+            return new Vec3(Plane.calcX(l2Eq, l1Eq.z), 0, l1Eq.z);
+        } else if (l2Eq.x === 1 && l2Eq.y === 0) {
+            return new Vec3(l2Eq.z, 0, Plane.calcY(l1Eq, l2Eq.z));
+        } else if (l2Eq.x === 0 && l2Eq.y === 1) {
+            return new Vec3(Plane.calcX(l1Eq, l2Eq.z), 0, l2Eq.z);
+        } else {
+            const x1 = 1.0;
+            const x2 = 5.0;
+            const y1 = Plane.calcY(l1Eq, x1);
+            const y2 = Plane.calcY(l1Eq, x2);
+
+            const x3 = 4.0;
+            const x4 = 8.0;
+            const y3 = Plane.calcY(l2Eq, x3);
+            const y4 = Plane.calcY(l2Eq, x4);
+
+            // http://mf-atelier.sakura.ne.jp/mf-atelier/modules/tips/program/algorithm/a1.html
+            const ksi   = ( y4-y3 )*( x4-x1 ) - ( x4-x3 )*( y4-y1 );
+            // const eta   = ( x2-x1 )*( y4-y1 ) - ( y2-y1 )*( x4-x1 );
+            const delta = ( x2-x1 )*( y4-y3 ) - ( y2-y1 )*( x4-x3 );
+
+            const lambda = ksi / delta;
+            // const mu    = eta / delta;
+
+            return new Vec3(x1 + lambda*( x2-x1 ), 0, y1 + lambda*( y2-y1 ));
+        }
+    }
+
+    /**
+     *
+     * @param {Vec2} p1
+     * @param {Vec2} p2
+     * @return {Vec3}
+     */
+    static computeLineEquation(p1, p2) {
+        const xDiff = p2.x - p1.x;
+        const yDiff = p2.y - p1.y;
+        if (Math.abs(xDiff) < 0.000001) {
+            // x = c
+            return new Vec3(1, 0, p1.x);
+        } else if (Math.abs(yDiff) < 0.000001) {
+            // y = c
+            return new Vec3(0, 1, p1.y);
+        } else {
+            // y = ax + b
+            return new Vec3(yDiff / xDiff, p1.y - p1.x * (yDiff / xDiff), 0);
+        }
+    }
+
+    /**
+     *
+     * @param {Vec3} line
+     * @param {number} y
+     * @return {number}
+     */
+    static calcX(line, y) {
+        if (line.z === 0) {
+            return (y - line.y) / line.x;
+        } else {
+            return line.z;
+        }
+    }
+
+    /**
+     *
+     * @param {Vec3} line
+     * @param {number} x
+     * @return {number}
+     */
+    static calcY(line, x) {
+        if (line.z === 0) {
+            return line.x * x + line.y;
+        } else {
+            return line.z;
+        }
+    }
+}
---
+++ src/geometry/sphere.js
@@ -0,0 +1,128 @@
+import Vec3 from '../vector3d.js';
+
+export default class Sphere {
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     * @param {number} z
+     * @param {number} r
+     */
+    constructor (x, y, z, r) {
+        this.center = new Vec3(x, y, z);
+        this.r = r;
+    }
+
+    /**
+     *
+     * @param {Vec3} p
+     * @returns {Vec3}
+     */
+    invertOnPoint(p) {
+        const d = p.sub(this.center);
+        const len = d.length();
+        return d.scale(this.rSq / (len * len)).add(this.center);
+    }
+
+    /**
+     *
+     * @param {Plane} plane
+     * @returns {Sphere}
+     */
+    invertOnPlane(plane) {
+        return Sphere.fromPoints(this.invertOnPoint(plane.p1),
+                                 this.invertOnPoint(plane.p2),
+                                 this.invertOnPoint(plane.p3),
+                                 this.center);
+    }
+
+    /**
+     *
+     * @param {} mat
+     * @param {number} n
+     * @param {number} k
+     * @returns {}
+     */
+    static pivoting(mat, n, k) {
+        let col = k;
+        let maxValue = Math.abs(mat[k][k]);
+        for (let i = k + 1; i < n; i++) {
+            if (Math.abs(mat[i][k]) > maxValue) {
+                col = i;
+                maxValue = Math.abs(mat[i][k]);
+            }
+        }
+        if (k !== col) {
+            var tmp = mat[col];
+            mat[col] = mat[k];
+            mat[k] = tmp;
+        }
+        return mat;
+    }
+
+    /**
+     *
+     * @param {Vec3} p1
+     * @param {Vec3} p2
+     * @param {Vec3} p3
+     * @param {Vec3} p4
+     * @returns {Sphere}
+     */
+    static fromPoints(p1, p2, p3, p4) {
+        const p = [p1, p2, p3, p4];
+        let coefficient = [[], [], []];
+        for (let i = 0; i < 3; i++) {
+            coefficient[i][0] = 2 * (p[i + 1].x - p[i].x);
+            coefficient[i][1] = 2 * (p[i + 1].y - p[i].y);
+            coefficient[i][2] = 2 * (p[i + 1].z - p[i].z);
+            coefficient[i][3] = -(Math.pow(p[i].x, 2) + Math.pow(p[i].y, 2) + Math.pow(p[i].z, 2)) +
+                Math.pow(p[i + 1].x, 2) + Math.pow(p[i + 1].y, 2) + Math.pow(p[i + 1].z, 2);
+        }
+
+        // Gaussian elimination
+        // Implementation is based on http://www.slis.tsukuba.ac.jp/~fujisawa.makoto.fu/cgi-bin/wiki/index.php?%A5%D4%A5%DC%A5%C3%A5%C8%C1%AA%C2%F2
+        // forward elimination
+        const n = 3;
+        for (let k = 0; k < n - 1; k++) {
+            coefficient = Sphere.pivoting(coefficient, n, k);
+
+            const vkk = coefficient[k][k];
+            for (let i = k + 1; i < n; i++) {
+                const vik = coefficient[i][k];
+                for (let j = k; j < n + 1; ++j) {
+                    coefficient[i][j] = coefficient[i][j] - vik * (coefficient[k][j] / vkk);
+                }
+            }
+        }
+
+        // back substitution
+        coefficient[n - 1][n] = coefficient[n - 1][n] / coefficient[n - 1][n - 1];
+        for (let i = n - 2; i >= 0; i--) {
+            let acc = 0.0;
+            for (let j = i + 1; j < n; j++) {
+                acc += coefficient[i][j] * coefficient[j][n];
+            }
+            coefficient[i][n] = (coefficient[i][n] - acc) / coefficient[i][i];
+        }
+
+        const center = new Vec3(coefficient[0][3], coefficient[1][3], coefficient[2][3]);
+        const r = center.sub(p1).length();
+        return new Sphere(center.x, center.y, center.z, r);
+    }
+
+    /**
+     *
+     * @param {Sphere} invertSphere
+     * @returns {Sphere}
+     */
+    invertOnSphere(invertSphere) {
+        const r = invertSphere.r;
+        const RT_3 = Math.sqrt(3);
+        const coeffR = r * RT_3 / 3;
+        const p1 = this.invertOnPoint(invertSphere.center.add(new Vec3(coeffR, coeffR, coeffR)));
+        const p2 = this.invertOnPoint(invertSphere.center.add(new Vec3(-coeffR, -coeffR, -coeffR)));
+        const p3 = this.invertOnPoint(invertSphere.center.add(new Vec3(coeffR, -coeffR, -coeffR)));
+        const p4 = this.invertOnPoint(invertSphere.center.add(new Vec3(coeffR, coeffR, -coeffR)));
+        return Sphere.fromPoints(p1, p2, p3, p4);
+    }
+}
